d7b3518fb774f41cad5821ce6f1706721eee15b9
Dariusz Okoński <darek200180@gmail.com>
2024-02-27 21:23:28 +0100

    add title page

--- src/MovieDetails.jsx
+++ src/MovieDetails.jsx
@@ -55,6 +55,15 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     getMovieDetails();
   }, [selectedId]);
 
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+
+    return () => {
+      document.title = 'usePopcorn';
+    };
+  }, [title]);
+
   return (
     <div className='details'>
       {isLoading ? (
