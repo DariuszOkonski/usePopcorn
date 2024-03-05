774918ed660a27a18715e428fb448c0b1340d40b
Dariusz Okoński <darek200180@gmail.com>
2024-03-05 19:37:21 +0100

    add focus on search input

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 function Search({ query, setQuery }) {
+  // useEffect(() => {
+  //   const el = document.querySelector('.search');
+  //   console.log(el);
+  //   el.focus();
+  // }, []);
+
+  const inputElement = useRef(null);
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === inputElement.current) {
+        return;
+      }
+
+      if (e.code === 'Enter') {
+        inputElement.current.focus();
+        setQuery('');
+      }
+    }
+    document.addEventListener('keydown', callback);
+
+    return () => document.removeEventListener('keydown', callback);
+  }, []);
+
   return (
     <input
+      ref={inputElement}
       className='search'
       type='text'
       placeholder='Search movies...'
