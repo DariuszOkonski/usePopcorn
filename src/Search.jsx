696bebae2af36c4e022bb01995776ba6e8c88043
Dariusz Okoński <darek200180@gmail.com>
2024-03-05 19:39:28 +0100

    add dependencie

--- src/Search.jsx
+++ src/Search.jsx
@@ -22,7 +22,7 @@ function Search({ query, setQuery }) {
     document.addEventListener('keydown', callback);
 
     return () => document.removeEventListener('keydown', callback);
-  }, []);
+  }, [setQuery]);
 
   return (
     <input
